1dfd20429561f6627b77480852747682579938e0
4hgann <henry.gann@gmail.com>
2021-10-12 10:08:26 +1300

    Now displays error if repo not found.

--- API.js
+++ API.js
@@ -24,11 +24,18 @@ const questions = [
 
         const response = await prompts(questions);
 
-        console.log('Excellent! Querying '+response['repo_owner']+'/'+response['repo_name']+' for open PRs');
+        console.log('Excellent! Querying '+response['repo_owner']+'/'+response['repo_name']+'for open PRs');
         // Create a new request object
         while (pageRequests != 0) {
             const msg = 'https://api.github.com/repos/'+response['repo_owner']+'/'+response['repo_name']+'/pulls?per_page=100&page='+pagenumber;
-            await fetch(msg).then(res => res.json()).then(res => {
+            await fetch(msg).then(res => {
+                    if(!res.ok){
+                        throw Error(response.statusText);
+                    }
+                    else{
+                        return res.json();
+                    }
+                }).then(res => {
                 pageRequests = Object.keys(res).length;
                 requests += pageRequests;
                 pagenumber++;
