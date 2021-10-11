3933c7aec017339e1c9c2a2c783efce06959d30c
4hgann <henry.gann@gmail.com>
2021-10-11 22:11:44 +1300

    Can get pull requests. Will add functionality for failing requests soon.

--- API.js
+++ API.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 import prompts from 'prompts';
 
 const questions = [
@@ -16,7 +17,28 @@ const questions = [
 
 
 (async () => {
+    try{
+        var pagenumber=1;
+        var requests =0;
+        var pageRequests=1;
 
         const response = await prompts(questions);
+
+        console.log('Excellent! Querying '+response['repo_owner']+'/'+response['repo_name']+' for open PRs');
+        // Create a new request object
+        while (pageRequests != 0) {
+            const msg = 'https://api.github.com/repos/'+response['repo_owner']+'/'+response['repo_name']+'/pulls?per_page=100&page='+pagenumber;
+            await fetch(msg).then(res => res.json()).then(res => {
+                pageRequests = Object.keys(res).length;
+                requests += pageRequests;
+                pagenumber++;
+            })
+        }
+        console.log('# of open PRs: ' + requests)
+        console.log('Bye!')
+    }
+     catch(e){
+        console.log('There was a problem completing the query. It\'s likely that this repo does not exist.')
+     }
 })();
 
